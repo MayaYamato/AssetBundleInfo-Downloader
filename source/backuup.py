1a2524e94e17fbb65e504b84b83588d350c69705
MayaYamato <gbp.0516@gmail.com>
2019-02-09 16:01:37 +0900

    test

--- source/backuup.py
+++ source/backuup.py
@@ -15,49 +15,16 @@ if not os.path.exists(download_dir_bgm):
 if not os.path.exists(download_dir_asset):
         os.makedirs(download_dir_asset)
 
-def download(url,dst_path):
+#tmp = input("Which do you want, music or asset?").rstrip()
+
+def download_bgm(url,dst_path):
     f5 = open(dst_path,'wb')
-    download = requests.get(url)
-    f5.write(download.content)
+    mp3down = requests.get(url)
+    f5.write(mp3down.content)
     f5.close()
 
-tmp = input("Which do you want, music or asset?").rstrip()
-
-if tmp = asset
-
-print('Loading file')
-with open('AssetBundleInfo') as lines:
-    for line in lines:
-        #折り返し,改行文字の削除
-        url = line.rstrip('\r\n')
-        #ファイルの保存名の設定
-        filename = os.path.basename(url)
-        #pathの結合 asset/+filename
-        dst_path = os.path.join(download_dir_asset, filename)
-        print(url)
-        #既存ファイルの存在判定
-        if os.path.exists(dst_path):
-            #10000回までfor同一ファイル保存を回避できる
-            for n in range (1, 10000):
-                #保存名をfilename+(n)に変更
-                new_filename = str(filename)  + '(' + str(n) + ')'
-                #dst_pathの再設定(ファイルネームの変更)
-                dst_path = os.path.join(download_dir_asset, new_filename)
-
-                if not os.path.exists(dst_path):
-                    download(url, dst_path)
-                    break
-
-                else:
-                    #既に重複ファイル回避がされていた場合さらにforを回す必要がある
-                    continue
-        else:
-            download(url, dst_path)
-
-if tmp = music
-
-print('Loading file')
-with open('BgmLists') as lines:
+print('AssetBundleInfoを読み込みます')
+with open('a') as lines:
     for line in lines:
         #折り返し,改行文字の削除
         url = line.rstrip('\r\n')
@@ -66,3 +33,21 @@ with open('BgmLists') as lines:
         #pathの結合 asset/+filename
         dst_path = os.path.join(download_dir_bgm, filename)
         print(url)
+        #既存ファイルの存在判定
+        if os.path.exists(dst_path):
+            #10000回までfor同一ファイル保存を回避できる
+            for n in range (1, 10000):
+                #保存名をfilename+(n)に変更
+                new_filename = str(filename)  + '(' + str(n) + ')'
+                #dst_pathの再設定(ファイルネームの変更)
+                dst_path = os.path.join(download_dir_bgm, new_filename)
+
+                if not os.path.exists(dst_path):
+                    download_bgm(url, dst_path)
+                    break
+
+                else:
+                    #既に重複ファイル回避がされていた場合さらにforを回す必要がある
+                    continue
+        else:
+            download_bgm(url, dst_path)
