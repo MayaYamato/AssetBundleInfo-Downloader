3f2b0dae58b19b145dc4ba2cbc8e921c43ef3a94
MayaYamato <gbp.0516@gmail.com>
2019-02-09 12:05:07 +0900

    test

--- source/bgmdownloader.py
+++ source/bgmdownloader.py
@@ -1,36 +1,30 @@
-import os a
-import urllib.error
-import urllib.request
+import os
+import requests
+import sys
+import fileinput
+import re
+import json
 
-#ダウンロードディレクトリの設定
-download_dir_asset = 'asset'
 download_dir_bgm = 'bgm'
 
+if not os.path.exists(download_dir_bgm):
+        os.makedirs(download_dir_bgm)
+
+def download_bgm(url,dst_path):
+    f5 = open(dst_path,'wb')
+    mp3down = requests.get(url)
+    f5.write(mp3down.content)
+    f5.close()
+
 print('AssetBundleInfoを読み込みます')
-
-#ダウンロードディレクトリがなければ新規作成する
-if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
-
-#ダウンロード関数の定義
-def download_asset(url, dst_path):
-    try:
-        data = urllib.request.urlopen(url).read()
-        with open(dst_path, mode="wb") as f:
-            f.write(data)
-    except urllib.error.URLError as e:
-        print(e)
-
-#一行ずつ繰り返し処理
-print('AssetBundleInfoを読み込みます')
-with open('AssetBundleInfo') as lines:
+with open('a') as lines:
     for line in lines:
         #折り返し,改行文字の削除
         url = line.rstrip('\r\n')
         #ファイルの保存名の設定
         filename = os.path.basename(url)
         #pathの結合 asset/+filename
-        dst_path = os.path.join(download_dir, filename)
+        dst_path = os.path.join(download_dir_bgm, filename)
         print(url)
         #既存ファイルの存在判定
         if os.path.exists(dst_path):
@@ -39,14 +33,14 @@ with open('AssetBundleInfo') as lines:
                 #保存名をfilename+(n)に変更
                 new_filename = str(filename)  + '(' + str(n) + ')'
                 #dst_pathの再設定(ファイルネームの変更)
-                dst_path = os.path.join(download_dir, new_filename)
+                dst_path = os.path.join(download_dir_bgm, new_filename)
 
                 if not os.path.exists(dst_path):
-                    download_asset(url, dst_path)
+                    download_bgm(url, dst_path)
                     break
 
                 else:
                     #既に重複ファイル回避がされていた場合さらにforを回す必要がある
                     continue
         else:
-            download_asset(url, dst_path)
+            download_bgm(url, dst_path)
