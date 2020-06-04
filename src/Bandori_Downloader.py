3252582a5d6dfc22ab6ca0c5b63eaf116f18e44f
Maya Yamato <43272974+MayaYamato@users.noreply.github.com>
2020-06-04 12:17:37 +0900

    Code reduction, partially redundant code replaced with functions

--- src/Bandori_Downloader.py
+++ src/Bandori_Downloader.py
@@ -5,6 +5,21 @@ import requests
 import urllib.error
 import urllib.request
 
+version = 2.0
+AWSURL = 'https://d2ktlshvcuasnf.cloudfront.net/Release/'
+BGMURL = 'https://res.bandori.ga/assets/sound/'
+GITHUBURL = 'https://raw.githubusercontent.com/MayaYamato/Bandori_Downloader/master/version'
+VERSIONURL = 'https://raw.githubusercontent.com/esterTion/bangdream_master_db_diff/master/!dataVersion.txt'
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+download_dir_asset = os.getcwd()+r'\asset'
+download_dir_bgm = os.getcwd()+r'\bgm'
+
+print('Bandori_Downloader v'+str(version)+'\n')
+print('Created By:VERSUS.log\n### Please comment or reply if you have any errors or qustions\n')
+print('Twitter:@72G_VS\nBlog URL:https://raspberrypi422.mydns.jp\nGithub Repository:https://github.com/MayaYamato/Bandori_Downloader')
+print('### Thanks to external links used in this tool ...\n')
+
 def download_asset(url, dst_path):
     try:
         data = urllib.request.urlopen(url).read()
@@ -21,95 +36,80 @@ def download_bgm(url,dst_path):
     except urllib.error.URLError as e:
         pass
 
-version = 2.0
-AWSURL = 'https://d2ktlshvcuasnf.cloudfront.net/Release/'
-BGMURL = 'https://res.bandori.ga/assets/sound/'
-GITHUBURL = 'https://raw.githubusercontent.com/MayaYamato/Bandori_Downloader/master/version'
-VERSIONURL = 'https://raw.githubusercontent.com/esterTion/bangdream_master_db_diff/master/!dataVersion.txt'
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-download_dir_asset = os.getcwd()+r'\asset'
-download_dir_bgm = os.getcwd()+r'\bgm'
-
-print('Bandori_Downloader v'+str(version)+'\n')
-print('Created By:VERSUS.log\n### Please comment or reply if you have any errors or qustions\n')
-print('Twitter:@72G_VS\nBlog URL:https://raspberrypi422.mydns.jp\nGithub Repository:https://github.com/MayaYamato/Bandori_Downloader')
-print('### Thanks to external links used in this tool ...\n')
-
-#Version Check
-print('Version Checking ... ')
-with urllib.request.urlopen(GITHUBURL) as response:
-    html = response.read().decode() #responseで得たbyte列を変換
-    exever = html[:-1]
-
-#Version Update
-if float(exever) - float(version) > 0:
-    print('New Version Released:'+str(exever)+'\n')
-    tmp =input('Do you want to upgrade? yes or no\n>>')
-    if tmp == 'yes':
-        filename = os.path.basename('Bandori_Downloader ver:'+str(exever)+'.rar')
-        dst_path = os.path.join(os.getcwd(), filename)
-        download_bgm(r'https://github.com/MayaYamato/Bandori_Downloader/releases/download/v'+str(exever)+r'/Bandori_Downloader.exe',dst_path)
-        print('Download Complete')
-        exit()
-    elif tmp == 'no':
-        pass
+def Version_Check_Update(url,dst_path):
+    print('Version Checking ... ')
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode() #responseで得たbyte列を変換
+        exever = html[:-1]
+    if float(exever) - float(version) > 0:
+        print('New Version Released:'+str(exever)+'\n')
+        tmp =input('Do you want to upgrade? yes or no\n>>')
+        if tmp == 'yes':
+            filename = os.path.basename('Bandori_Downloader ver:'+str(exever)+'.rar')
+            dst_path = os.path.join(os.getcwd(), filename)
+            download_bgm(r'https://github.com/MayaYamato/Bandori_Downloader/releases/download/v'+str(exever)+r'/Bandori_Downloader.exe',dst_path)
+            print('Download Complete')
+            exit()
+        elif tmp == 'no':
+            pass
+        else:
+            pass
     else:
-        pass
-else:
-    print('Latest Version\n')
+        print('Latest Version\n')
 
-#Asset or BGM
-tmp = input('Which do you want Asset or BGM? \n (Asset ⇒ 1 Bgm ⇒ 2 )\n>>').rstrip()
+def ABI_Download(ver,tmp,OS,dst_path):
+    print('\nABI Downloading:')
+    if len(tmp) > 2:
+        url = AWSURL +str(ver)+str(tmp)+'/'+str(OS)+r'/AssetBundleInfo'
+    else:
+        url = AWSURL +str(ver)+'/'+str(OS)+r'/AssetBundleInfo'
+    download_asset(url, dst_path)
+    print('ABI Download complete\nShaping ABI started')
 
-#ABI/Asset Download
-if int(tmp) == 1:
+### main source ###
+
+### Asset or BGM ###
+tmp = input('Which do you want Asset or BGM? \n (Asset ⇒ 0 Bgm ⇒ 1 )\n>>').rstrip()
+
+### ABI/Asset Download ###
+if int(tmp) == 0:
 
     if not os.path.exists(download_dir_asset):
         os.makedirs(download_dir_asset)
 
-    #ABI Version
-    tmpex =input('\nWhat version of ABI do you want?\n ex:latest version ⇒ 0\n ex:2.0.0.300(18 Aprir) ⇒ 1\n ex:3.0.0.500(19 Aprir) ⇒ 2\n ex:Direct Input\n>>').rstrip()
-    if int(tmpex) == 0:
+    ### ABI Version ###
+    tmp =input('\nWhat version of ABI do you want?\n ex:latest version ⇒ 0\n ex:2.0.0.300(18 Aprir) ⇒ 1\n ex:3.0.0.500(19 Aprir) ⇒ 2\n ex:Direct Input\n>>').rstrip()
+    if int(tmp) == 0:
         with urllib.request.urlopen(VERSIONURL) as response:
             html = response.read().decode() #responseで得たbyte列を変換
             ver = html[:-1]
         print(ver)
-
-    elif int(tmpex) == 1:
+    elif int(tmp) == 1:
         ver = '2.0.0.300'
-    elif int(tmpex) == 2:
+    elif int(tmp) == 2:
         ver = '3.0.0.500'
     else :
-        ver = tmpex
+        ver = tmp
 
-    #OS Version
-    tmp = input("\nWhich do you want Android or iOS? \n(Android ⇒ 1 iOS ⇒ 2 )\n>> ").rstrip()
-    if int(tmp)==1:
+    ### OS Version ###
+    tmp = input("\nWhich do you want Android or iOS? \n(Android ⇒ 0 iOS ⇒ 1 )\n>> ").rstrip()
+    if int(tmp)==0:
         OS='Android'
-    elif int(tmp)==2:
+    elif int(tmp)==1:
         OS='iOS'
     else :
         print('ERROR')
 
-    #ABI Shaping
-    tmp = input("\nChoose Shaping ABI method\n###Choose latest if you want to format the latest ABI,\n###OLD is effective against ABI before 2020\n(NEW ⇒ 0 OLD ⇒ 1)\n>> ").rstrip()
+    ### ABI Shaping ###
+    tmp = input("\nChoose Shaping ABI method\n###Choose Latest if you want to format the latest ABI,\n###Old is effective against ABI before 2020\n(Latest ⇒ 0 Old ⇒ 1)\n>> ").rstrip()
     
     if int(tmp)==0:
         tmp = input("Please enter the URL change character string. \n(This character string can be confirmed by packet capture etc. If you do not understand the meaning, ask the author)\nforexample:_p3HzsCWjkY\n>>").rstrip()
-        
-        #ABI Download
-        print('\nABI Downloading:')
         filename = os.path.basename('AssetBundleInfo(raw) ver '+str(ver)+' .txt')
-        if int(tmpex) == 0:
-            url = AWSURL +str(ver)+str(tmp)+'/'+str(OS)+r'/AssetBundleInfo'
-        else:
-            url = AWSURL +str(ver)+'/'+str(OS)+r'/AssetBundleInfo'
         dst_path = os.path.join(download_dir_asset, filename)
-        download_asset(url, dst_path)
-        print('ABI Download complete\nShaping ABI started')
-
-        #latest ABI Shaping
+        ABI_Download(ver,tmp,OS,dst_path)
+    
+        ### latest ABI Shaping ###
         if os.path.exists(os.getcwd()+r'\asset\AssetBundleInfo ver '+str(ver)+' .txt'):
             os.remove(os.getcwd()+r'\asset\AssetBundleInfo ver '+str(ver)+' .txt')
         with codecs.open(r'asset/AssetBundleInfo(raw) ver '+str(ver)+' .txt',"r","cp932","ignore") as lines:
@@ -124,18 +124,11 @@ if int(tmp) == 1:
                     f.write(new_txt)
 
     elif int(tmp)==1:
-
-        #ABI Download
-        print('\nABI Downloading:')
         filename = os.path.basename('AssetBundleInfo(raw) ver '+str(ver)+' .txt')
-        if int(tmpex) == 0:
-            url = AWSURL +str(ver)+'/'+str(OS)+r'/AssetBundleInfo'
-        else:
-            url = AWSURL +str(ver)+'/'+str(OS)+r'/AssetBundleInfo'
         dst_path = os.path.join(download_dir_asset, filename)
-        download_asset(url, dst_path)
+        ABI_Download(ver,tmp,OS,dst_path)
 
-        #OLD ABI Shaping
+        ### OLD ABI Shaping ###
         if os.path.exists(os.getcwd()+r'\asset\AssetBundleInfo ver '+str(ver)+' .txt'):
             os.remove(os.getcwd()+r'\asset\AssetBundleInfo ver '+str(ver)+' .txt')
         with codecs.open(r'asset/AssetBundleInfo(raw) ver '+str(ver)+' .txt',"r","cp932","ignore") as lines:
@@ -149,14 +142,13 @@ if int(tmp) == 1:
                 with open(r'asset\AssetBundleInfo ver '+str(ver)+' .txt',"a") as f:
                     f.write(new_txt)
     else :
-        print('ERROR')
+        print("ERROR")
 
-    print('Complete\n')
+    print("Complete\n")
 
-    #All Asset Download
-    print('Do you want to download all asset?\n### But not recommended to download:Too many assets. yes or no')
-    tmp=input('>>')
-    if tmp == 'yes' :
+    ### All Asset Download ###
+    tmp = input("Do you want to download all asset?\n### But not recommended to download:Too many assets. yes ⇒ 0 no ⇒ 1\n>>")
+    if int(tmp) == 0 :
         print('Loading ABI')
         with open(r'asset/AssetBundleInfo ver '+str(ver)+' .txt') as lines:
             for line in lines:
@@ -174,14 +166,14 @@ if int(tmp) == 1:
                             continue
                 else:
                     download_asset(url, dst_path)
-        print('Download complete')
-    elif tmp == 'no' :
+        print("Download complete")
+    elif int(tmp) == 1 :
         exit()
     else:
         exit()
 
-#BGM Download
-elif int(tmp) == 2:
+### BGM Download ###
+elif int(tmp) == 1:
 
     if not os.path.exists(download_dir_bgm):
         os.makedirs(download_dir_bgm)
